db1d87f69fa2acf0894324fe946450adafa15c56
maksim <bakanovmp12@gmail.com>
2021-04-25 13:55:36 +0300

    Bakanov Maksim 381707-3

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,31 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task, test1)
+{
+    Automata automata = Automata();
+    automata.on();
+    automata.coin(0);
+    automata.choise(1);
+    States result = automata.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task, test2)
+{
+    Automata automata = Automata();
+    automata.on();
+    automata.choise(1);
+    States result = automata.getState();
+    EXPECT_EQ(WAIT, result);
+}
+
+TEST(task, test3)
+{
+    Automata automata = Automata();
+    automata.on();
+    automata.coin(150);
+    automata.choise(0);
+    int result = automata.finish();
+    EXPECT_EQ(50, result);
+}
