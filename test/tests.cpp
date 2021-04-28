1b786042f2c68da1b266b406078a63f8a6895c01
Maksim Bakanov <59537014+BakanovMaksim@users.noreply.github.com>
2021-04-28 10:40:05 +0300

    Update tests.cpp
    fix tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,29 +3,29 @@
 
 TEST(task, test1)
 {
-    Automata automata = Automata();
-    automata.on();
-    automata.coin(0);
-    automata.choise(1);
-    States result = automata.getState();
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(0);
+    coffee.choise(1);
+    States result = coffee.getState();
     EXPECT_EQ(WAIT, result);
 }
 
 TEST(task, test2)
 {
-    Automata automata = Automata();
-    automata.on();
-    automata.choise(1);
-    States result = automata.getState();
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.choise(1);
+    States result = coffee.getState();
     EXPECT_EQ(WAIT, result);
 }
 
 TEST(task, test3)
 {
-    Automata automata = Automata();
-    automata.on();
-    automata.coin(150);
-    automata.choise(0);
-    int result = automata.finish();
-    EXPECT_EQ(50, result);
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(20);
+    coffee.choise(1);
+    int result = coffee.finish();
+    EXPECT_EQ(10, result);
 }
